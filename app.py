349110aac18cb7b40ad634f3915d568294fae707
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2023-03-02 23:46:13 +0100

    balazs-matyassy: repository

--- app.py
+++ app.py
@@ -1,12 +1,74 @@
+import os
+
 from flask import Flask
 
+from repository.products import load_all_products, load_product, line_to_product, save_product, delete_product, \
+    setup_products
+
+
+PRODUCTS_PATH = None
+
+
+def setup():
+    global PRODUCTS_PATH
+
+    os.makedirs(app.instance_path, exist_ok=True)
+    PRODUCTS_PATH = os.path.join(app.instance_path, 'products.csv')
+    setup_products(PRODUCTS_PATH)
+
+
+def input_cmd():
+    line = input('>> ').strip()
+    values = line.split(' ', 1)
+
+    return {
+        'name': values[0].strip(),
+        'param': values[1] if len(values) >= 2 else None
+    }
+
+
 app = Flask(__name__)
+setup()
 
+print('==================================================')
+print('\tWelcome to SpacePort 0.1')
+print('==================================================')
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+cmd = input_cmd()
 
+while cmd['name'] != 'exit':
+    if cmd['name'] == 'list':
+        products = load_all_products(PRODUCTS_PATH)
 
-if __name__ == '__main__':
-    app.run()
+        print(f'{"ID":4} {"Name":32} {"Unit price":8}')
+
+        for product in products:
+            print(f'{product["id"]:4} {product["name"]:32} {product["unit_price"]:8}')
+    elif cmd['name'] == 'view':
+        product = load_product(PRODUCTS_PATH, int(cmd['param']))
+        print(f'{"ID":4} {"Name":32} {"Unit price":8}')
+        print(f'{product["id"]:4} {product["name"]:32} {product["unit_price"]:8}')
+    elif cmd['name'] == 'save':
+        product = line_to_product(cmd['param'], delimiter=',')
+        save_product(PRODUCTS_PATH, product)
+        print('Product saved.')
+    elif cmd['name'] == 'delete':
+        delete_product(PRODUCTS_PATH, int(cmd['param']))
+        print('Product deleted.')
+    elif cmd['name'] == 'help':
+        print('Available commands:')
+        print('- list')
+        print('- view [id]')
+        print('- save [name],[unit_price]')
+        print('\t-> create new product')
+        print('- save [id],[name],[unit_price]')
+        print('\t-> update existing product')
+        print('- delete [id]')
+        print('- help')
+        print('- exit')
+    else:
+        print('Wrong command!')
+
+    cmd = input_cmd()
+
+print('Bye! :)')
---
+++ repository/products.py
@@ -0,0 +1,131 @@
+import os
+
+
+def product_header(delimiter=';'):
+    return 'id' \
+        + delimiter + 'name' \
+        + delimiter + 'unit_price'
+
+
+def setup_products(path, delimiter=';'):
+    if not os.path.isfile(path):
+        with open(path, 'w', encoding='utf-8') as file:
+            header = product_header(delimiter)
+            file.write(f'{header}\n')
+
+
+def line_to_product(line, delimiter=';'):
+    line = line.strip()
+    values = line.split(delimiter)
+
+    if len(values) >= 3:
+        return {
+            'id': int(values[0].strip()),
+            'name': values[1].strip(),
+            'unit_price': int(values[2].strip())
+        }
+    else:
+        return {
+            'id': None,
+            'name': values[0].strip(),
+            'unit_price': int(values[1].strip())
+        }
+
+
+def product_to_line(product, delimiter=';'):
+    return str(product['id']) \
+        + delimiter + product['name'] \
+        + delimiter + str(product['unit_price'])
+
+
+def load_all_products(path, delimiter=';'):
+    products = []
+
+    with open(path, encoding='utf-8') as file:
+        file.readline()
+
+        for line in file:
+            product = line_to_product(line, delimiter)
+            products.append(product)
+
+        return products
+
+
+def save_all_products(path, products, delimiter=';'):
+    with open(path, 'w', encoding='utf-8') as file:
+        header = product_header(delimiter)
+        file.write(f'{header}\n')
+
+        for product in products:
+            line = product_to_line(product, delimiter)
+            file.write(f'{line}\n')
+
+
+def load_product(path, product_id, delimiter=';'):
+    products = load_all_products(path, delimiter)
+
+    for product in products:
+        if product['id'] == product_id:
+            return product
+
+    return None
+
+
+def create_product(path, product, delimiter=';'):
+    products = load_all_products(path, delimiter)
+    max_id = 0
+
+    for stored_product in products:
+        if stored_product['id'] > max_id:
+            max_id = stored_product['id']
+
+    product['id'] = max_id + 1
+
+    with open(path, 'a', encoding='utf-8') as file:
+        line = product_to_line(product, delimiter)
+        file.write(f'{line}\n')
+
+    return product
+
+
+def update_product(path, product, delimiter=';'):
+    products = load_all_products(path, delimiter)
+    updated_product = None
+
+    for stored_product in products:
+        if stored_product['id'] == product['id']:
+            stored_product['name'] = product['name']
+            stored_product['unit_price'] = product['unit_price']
+            updated_product = stored_product
+            break
+
+    if updated_product is not None:
+        save_all_products(path, products, delimiter)
+
+    return updated_product
+
+
+def save_product(path, product, delimiter=';'):
+    if 'id' not in product or product['id'] is None:
+        # CREATE
+        return create_product(path, product, delimiter)
+    else:
+        # UPDATE
+        return update_product(path, product, delimiter)
+
+
+def delete_product(path, product_id, delimiter=';'):
+    products = load_all_products(path, delimiter)
+    filtered = []
+    deleted_product = None
+
+    for product in products:
+        if product['id'] != product_id:
+            filtered.append(product)
+        else:
+            deleted_product = product
+
+    if deleted_product is not None:
+        save_all_products(path, filtered)
+
+    return deleted_product
