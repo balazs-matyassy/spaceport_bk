19b9c6ccd830360abcc34209cce262a8500853e7
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2023-03-02 23:57:06 +0100

    balazs-matyassy: create

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
-from repository.products import load_all_products, setup_products
+from repository.products import load_all_products, setup_products, save_product
 
 PRODUCTS_PATH = None
 
@@ -29,6 +29,24 @@ def home():
 def list_products():
     products = load_all_products(PRODUCTS_PATH)
     return render_template('products/list.html', products=products)
+
+
+@app.route('/products/create', methods=("GET", "POST"))
+def create_product():
+    product = {
+        'name': '',
+        'unit_price': 0
+    }
+
+    if request.method == "POST":
+        product['name'] = request.form['name']
+        product['unit_price'] = int(request.form['unit_price'])
+        save_product(PRODUCTS_PATH, product)
+
+        return redirect(url_for("list_products"))
+
+    return render_template(
+        'products/create.html', product=product)
 
 
 if __name__ == '__main__':
