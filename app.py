2562c5dd36b5c31e4b5d1468549035c2346beab5
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2023-03-08 01:18:06 +0100

    balazs-matyassy: repository class

--- app.py
+++ app.py
@@ -29,10 +29,14 @@ def products_create():
         product.name = request.form['name']
         product.unit_price = int(request.form['unit_price'])
         product.discount = int(request.form['discount'])
-        product_repository.save(product)
-        flash('Product created.')
 
-        return redirect(url_for("products_list"))
+        if product.name != '':
+            product_repository.save(product)
+            flash('Product created.')
+
+            return redirect(url_for("products_list"))
+        else:
+            flash('Name missing.')
 
     return render_template(
         'products/edit.html',
@@ -49,8 +53,12 @@ def products_edit(product_id):
         product.name = request.form['name']
         product.unit_price = int(request.form['unit_price'])
         product.discount = int(request.form['discount'])
-        product_repository.save(product)
-        flash('Product saved.')
+
+        if product.name != '':
+            product_repository.save(product)
+            flash('Product saved.')
+        else:
+            flash('Name missing.')
 
     return render_template(
         'products/edit.html',
